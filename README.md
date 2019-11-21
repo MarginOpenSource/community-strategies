ceb58c278bcc7285f863c1d7708ca7268548bb49
Thoren Huppke <19409731+THuppke@users.noreply.github.com>
2019-11-21 16:56:55 +0100

    Adding Python structure, setup and README file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# community-strategies
+# Community Strategies
+
+This repository contains community-created strategies for the Python Strategy Editor of the margin
+application found at [margin.io]().
+
+This package is available for Python 3.6 or higher through pip
+```
+pip install margin-community-strategies
+```
+
+If you want to write your own strategies, we recommend installing the strategy SDK from
+https://github.com/MarginOpenSource/strategy-sdk or using pip
+```
+pip install margin-strategy-sdk
+```
+and start by cloning the template strategy from
+https://github.com/MarginOpenSource/strategy-template.
+
+Also check out the official strategies by margin at
+https://github.com/MarginOpenSource/official-strategies
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="margin_community_strategies",
+    version="0.0.1",
+    author="Margin UG",
+    author_email="contact@margin.io",
+    description="Strategies for the Margin Strategy Editor provided by the community",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarginOpenSource/community-strategies",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["margin-strategy-sdk"],
+    python_requires='>=3.6',
+)
